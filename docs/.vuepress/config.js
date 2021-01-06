a36d2d1176d7d707f8457ba7b0d6428c9d0f9f8a
yukiyang0729 <1072131118@qq.com>
2021-01-06 14:37:51 +0800

    feat: init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,10 @@
 module.exports = {
-    base: '/blog/', // 比如你的仓库是test
+    base: '/blog/',
     dest: 'dist',
-    title: '裂泉首页', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
-    description: '裂泉的前端记录', // meta 中的描述文字，用于SEO
-    // 注入到当前页面的 HTML <head> 中的标签
+    title: '青舟', 
+    description: '青舟博客',
     head: [
-        ['link', { rel: 'icon', href: '/egg.png' }],  //浏览器的标签栏的网页图标
+        ['link', { rel: 'icon', href: '/egg.png' }],
     ],
     markdown: {
         lineNumbers: true
@@ -13,34 +12,41 @@ module.exports = {
     serviceWorker: true,
     themeConfig: {    
         logo: '/egg.png',
-        lastUpdated: 'lastUpdate', // string | boolean
+        lastUpdated: 'lastUpdate',
         nav: [
             { text: '首页', link: '/' },
-            {
-                text: '分类',
-                ariaLabel: '分类',
-                items: [
-                    { text: '文章', link: '/pages/folder1/test1.md' },
-                    { text: '琐碎', link: '/pages/folder2/test4.md' },
-                ]
-            },
-            { text: '功能演示', link: '/pages/folder1/test3.md' },
-            { text: 'Github', link: 'https://github.com/dwanda' },
+            { text: 'Github', link: 'https://github.com/qingzhou729/blog' },
         ],
         sidebar: {
-            '/pages/folder1/':[
+            '/pages/react/':[
                 {
-                    title: '测试菜单1',   // 必要的
-                    collapsable: false, // 可选的, 默认值是 true,
-                    sidebarDepth: 1,    // 可选的, 默认值是 1
+                    title: 'react',
+                    collapsable: false,
+                    sidebarDepth: 1,
                     children: [
                         ['test1.md', '子菜单1'],
-                        ['test3.md', '子菜单2']
                     ]
                 },
                 {
                     title: '测试菜单2',
-                    collapsable: false, // 可选的, 默认值是 true,
+                    collapsable: false,
+                    children: [
+                        ['test2.md', '子菜单1']
+                    ]
+                }
+            ],
+            '/pages/vue/':[
+                {
+                    title: 'vue',
+                    collapsable: false,
+                    sidebarDepth: 1,
+                    children: [
+                        ['test1.md', '子菜单1'],
+                    ]
+                },
+                {
+                    title: '测试菜单2',
+                    collapsable: false,
                     children: [
                         ['test2.md', '子菜单1']
                     ]
